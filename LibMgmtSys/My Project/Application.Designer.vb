a90b299c7b6a12cfe15e6555d73902cefe324afa
Loximity-oss <67264198+Loximity-oss@users.noreply.github.com>
2020-09-04 22:20:08 +0800

    Renamed Form

--- LibMgmtSys/My Project/Application.Designer.vb
+++ LibMgmtSys/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.LibMgmtSys.Form1
+            Me.MainForm = Global.LibMgmtSys.Login
         End Sub
     End Class
 End Namespace
